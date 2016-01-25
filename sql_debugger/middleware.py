a9e7d050f14cfb95bf0b63aef28004395706ccce
katharine-kinn <katherine.goncharova@gmail.com>
2016-01-25 17:35:56 +0300

    added visualization for ajax requests that returned error

--- sql_debugger/middleware.py
+++ sql_debugger/middleware.py
@@ -10,12 +10,29 @@ class SQLDebugMiddleware(object):
             return response
 
         if request.is_ajax(): #TODO: if not JSON?
-            try:
-                resp_d = json.loads(response.content)
-                resp_d['path'] = request.get_full_path()
-                resp_d['sql_debug_info'] = connection.queries
-                response.content = json.dumps(resp_d)
-            except Exception, e:
-                pass       
+
+            if response.status_code / 100 == 2:
+                try:
+                    resp_d = json.loads(response.content)
+                    resp_d['path'] = request.get_full_path()
+                    resp_d['sql_debug_info'] = connection.queries
+                    response.content = json.dumps(resp_d)
+                except Exception, e:
+                    pass
+            else:
+                parts = {
+                    "traceback": "Traceback"
+                }
+
+                empty_line = '\n\n'
+                resp_parts = response.content.split(empty_line)
+                res = { "error": resp_parts[0] }
+
+                for rp in resp_parts:
+                    for k,p in parts.iteritems():
+                        if rp.startswith(p):
+                            res[k] = rp
+
+                response.content = json.dumps({"errordata": res, "path": request.get_full_path()})
 
         return response
